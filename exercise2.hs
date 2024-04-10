ea5e5e4a12c5579554496a07443c3f2771b84f4b
Johannes <33333384+JohannesDev@users.noreply.github.com>
2024-04-10 14:54:02 +0200

    Implement Cell equality and add basic tests for all Binoxxo grid generation functions

--- exercise2.hs
+++ exercise2.hs
@@ -1,16 +1,24 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Eta reduce" #-}
 
 import Data.Array
+import Data.List
 import Data.Maybe
 import Text.Printf
-import Data.List
 
 -- Task 1 ----------------------------------------------------------------------
 type Nat1 = Integer
 
 type Index = (Nat1, Nat1)
-data Cell = X | O | Empty
+
+data Cell = X | O | Empty deriving (Show)
+
+instance Eq Cell where
+  (==) X X = True
+  (==) O O = True
+  (==) Empty Empty = True
+  (==) _ _ = False
 
 type BinoxxoL = [[Cell]]
 
@@ -19,20 +27,20 @@ type BinoxxoF = Array Index Cell
 lengthAsInteger :: [a] -> Integer
 lengthAsInteger x = toInteger (length x)
 
-
 -- Task 2 ----------------------------------------------------------------------
 generiereBinoxxoL :: Index -> BinoxxoL -> BinoxxoL
 generiereBinoxxoL (rows, cols) cells = cells
 
-
-generiereBinoxxoF1 :: Index -> [(Index,Cell)] -> BinoxxoF
-generiereBinoxxoF1 (rows, cols) cells = array ((1,rows),(1,cols)) cells
+generiereBinoxxoF1 :: Index -> [(Index, Cell)] -> BinoxxoF
+generiereBinoxxoF1 (rows, cols) cells = array ((1, 1), (rows, cols)) cells
 
 generiereBinoxxoF2 :: Index -> [Cell] -> BinoxxoF
-generiereBinoxxoF2 (rows, cols) cells = listArray ((1,rows),(1,cols)) cells
+generiereBinoxxoF2 (rows, cols) cells = listArray ((1, 1), (rows, cols)) cells
 
-generiereBinoxxoF3 :: Index -> [(Index,Cell)] -> BinoxxoF
-generiereBinoxxoF3 (rows, cols) cells = accumArray (\a b -> b) Empty ((1, rows), (1, cols)) cells 
+-- aggregation function (\a b -> b) makes sure that if an index is defined multiple times,
+-- then the last specified value should be kept
+generiereBinoxxoF3 :: Index -> [(Index, Cell)] -> BinoxxoF
+generiereBinoxxoF3 (rows, cols) cells = accumArray (\a b -> b) Empty ((1, 1), (rows, cols)) cells
 
 -- TestSuite -------------------------------------------------------------------
 
@@ -50,7 +58,22 @@ assertContains testName haystack needle =
     else printf "\x1b[31mfailed\x1b[0m %s\n\tExpected Containing: %s\n\tActual: %s\n" testName (show needle) (show haystack)
 
 -- Runs all tests
---runTests :: IO ()
---runTests = do
+runTests :: IO ()
+runTests = do
   -- Exerise 1 tests --
---  assertEqual "generiereBinoxxoL 2/3" (generiereBinoxxoF1 (1,3) [(1,0),(2,1),(3,1)]) (array (1,3) [(1,0),(2,1),(3,1)])
+  assertEqual
+    "generiereBinoxxoL basic case"
+    (generiereBinoxxoL (2, 2) [[X, O], [X, Empty]])
+    [[X, O], [X, Empty]]
+  assertEqual
+    "generiereBinoxxoF1 basic case"
+    (generiereBinoxxoF1 (2, 2) [((1, 1), X), ((1, 2), O), ((2, 1), X), ((2, 2), Empty)])
+    (array ((1, 1), (2, 2)) [((1, 1), X), ((1, 2), O), ((2, 1), X), ((2, 2), Empty)])
+  assertEqual
+    "generiereBinoxxoF2 basic case"
+    (generiereBinoxxoF2 (2, 2) [X, O, X, Empty])
+    (array ((1, 1), (2, 2)) [((1, 1), X), ((1, 2), O), ((2, 1), X), ((2, 2), Empty)])
+  assertEqual
+    "generiereBinoxxoF3 basic case"
+    (generiereBinoxxoF3 (2, 2) [((1, 1), X), ((1, 2), O), ((1, 1), O), ((2, 1), X), ((2, 2), Empty)])
+    (array ((1, 1), (2, 2)) [((1, 1), O), ((1, 2), O), ((2, 1), X), ((2, 2), Empty)])
