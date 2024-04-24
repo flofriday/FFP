6346afb286822756cbcaa59130c0860391aa98ed
JohannesDev <jonny.bliha@gmail.com>
2024-04-24 12:20:21 +0200

    Basic Parser setup

--- exercise3.hs
+++ exercise3.hs
@@ -38,6 +38,52 @@ prop_quotientSmalleThanEpsilon i = actualDifference <= epsilon
     goldenRatio = (1 + sqrt 5) / 2
     actualDifference = abs (fibQuot - goldenRatio)
 
+
+
+-- MARK: Parser
+type Parse a b = [a] -> [(b,[a])]
+
+topLevel :: Parse a b -> [a] -> b
+topLevel p input
+  = case results of
+    [] -> error "parse unsuccessful"
+    _ -> head results
+  where
+    results = [ found | (found, []) <- p input ]
+
+parser :: Parse Char (Maybe String)
+
+
+none :: Parse a b
+none _ = []
+
+succeed :: b -> Parse a b
+succeed val inp = [(val,inp)]
+
+token :: Eq a => a -> Parse a a
+token t (x:xs)
+  | t == x = [(t,xs)]
+  | otherwise = []
+token t [] = []
+
+spot :: (a -> Bool) -> Parse a a
+spot p (x:xs)
+  | p x = [(x,xs)]
+  | otherwise = []
+spot p [] = []
+
+alt :: Parse a b -> Parse a b -> Parse a b
+alt p1 p2 input = p1 input ++ p2 input
+
+(>*>) :: Parse a b -> Parse a c -> Parse a (b,c)
+(>*>) p1 p2 input = [((y,z),rem2) | (y,rem1) <- p1 input,(z,rem2) <- p2 rem1 ]
+
+build :: Parse a b -> (b -> c) -> Parse a c
+build p f input = [ (f x,rem) | (x,rem) <- p input ]
+
+
+-- topLevel parser "PROGRAM someName SKIP"
+
 -- Runs all tests
 -- MARK: Tests
 -- runTests :: IO ()
