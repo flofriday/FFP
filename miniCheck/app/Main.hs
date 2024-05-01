0943ebfd5aaf0d99a649b1d57dbb454a73a247aa
Johannes <33333384+JohannesDev@users.noreply.github.com>
2024-05-01 11:17:08 +0200

    Define transition system module

--- miniCheck/app/Main.hs
+++ miniCheck/app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import System.IO
+
+import TransitionSystem
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    handle <- openFile "./app/transition_system.txt" ReadMode
+    contents <- hGetContents handle
+    putStrLn (parserTransitionSystem contents)
+    hClose handle
---
+++ miniCheck/app/TransitionSystem.hs
@@ -0,0 +1,4 @@
+module TransitionSystem (parserTransitionSystem) where
+
+parserTransitionSystem :: String -> String
+parserTransitionSystem x = "Parsed"
