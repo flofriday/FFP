a252d9b2e6456b82ad85415bf315e98ddce16453
flofriday <flohacksfriday@gmail.com>
2024-06-01 14:56:19 +0200

    Add more Mini-- compiler tests

--- miniCheck/test/MiniMMCompilerSpec.hs
+++ miniCheck/test/MiniMMCompilerSpec.hs
@@ -8,20 +8,35 @@ import MiniMM
 import MiniMMCompiler
 import Text.Parsec (parse)
 import Data.Either (isRight, isLeft, fromRight)
+import Data.Maybe (fromJust)
 import qualified Data.Either as Either
 import MiniMMCompiler (compileMiniMM)
 import TransitionSystem
+import TransitionSystem (TransitionSystem)
+import Control.Monad (when)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.List
 
+-- | Counts the number of nodes/states in the transition system
 nodeCount :: TransitionSystem -> Int
 nodeCount ts = length $ states ts
 
+-- | Counts the number of transitions in the transition system
 transCount :: TransitionSystem -> Int
 transCount ts = length $ transition ts
 
+-- | Tests wether a atomic proposition is set in all return nodes
+isInAllReturns :: AP -> TransitionSystem -> Bool
+isInAllReturns target ts = all (elem target) returnAps
+    where 
+        returnAps = map (\r -> fromJust (Map.lookup r (label_functions ts))) returnNodes
+        returnNodes = filter (\s -> "return" `isPrefixOf` s) (Set.toList (states ts))
 
 spec :: Spec
 spec = do
-  describe "compile without check" $ do
+  describe "compile without checking ts" $ do
     it "minimal example" $ do
         let src = [r|
             procedure main(a) {
@@ -192,6 +207,76 @@ spec = do
         nodeCount ts `shouldBe` 8
         transCount ts `shouldBe` 8
 
+    it "readbool" $ do
+        let src = [r|
+           procedure main(a) {
+                b = read_bool();
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        nodeCount ts `shouldBe` 9
+        transCount ts `shouldBe` 11 
+
+    it "2x readbool" $ do
+        let src = [r|
+           procedure main(a) {
+                b = read_bool();
+                c = read_bool();
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        nodeCount ts `shouldBe` 17
+        transCount ts `shouldBe` 23
+
+    it "3x readbool" $ do
+        let src = [r|
+           procedure main(a) {
+                b = read_bool();
+                c = read_bool();
+                d = read_bool();
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        nodeCount ts `shouldBe` 33 
+        transCount ts `shouldBe` 47
+
+    it "4x readbool" $ do
+        let src = [r|
+           procedure main(a) {
+                b = read_bool();
+                c = read_bool();
+                d = read_bool();
+                e = read_bool();
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        nodeCount ts `shouldBe` 65 
+        transCount ts `shouldBe` 95
+
+    it "5x readbool" $ do
+        let src = [r|
+           procedure main(a) {
+                b = read_bool();
+                c = read_bool();
+                d = read_bool();
+                e = read_bool();
+                f = read_bool();
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        nodeCount ts `shouldBe` 129 
+        transCount ts `shouldBe` 191
+
     it "assignmentExample" $ do
         let src = [r|
            procedure main(a, b) {
@@ -206,3 +291,192 @@ spec = do
         nodeCount ts `shouldBe` 21
         transCount ts `shouldBe` 21
     
+
+  {- During compilaion all expressions are evaluated and in these tests we 
+     verify that the evaluation is correct, by looking into all returns in the 
+     end.
+  -}
+  describe "compile and verify evaluated expressions" $ do
+    it "simple assign" $ do
+        let src = [r|
+           procedure main(a) {
+                x = false;
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("x", False) ts `shouldBe` True
+
+    it "if then branch" $ do
+        let src = [r|
+           procedure main(a) {
+                cond = true;
+                if (cond) {
+                    x = true;
+                }
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("x", True) ts `shouldBe` True
+
+    it "if no branch" $ do
+        let src = [r|
+           procedure main(a) {
+                cond = false;
+                x = false;
+                if (cond) {
+                    x = true;
+                }
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("x", False) ts `shouldBe` True
+
+
+    it "if else then branch" $ do
+        let src = [r|
+           procedure main(a) {
+                if (true) {
+                    x = true;
+                } else {
+                    x = false;
+                }
+                return x;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("x", True) ts `shouldBe` True
+
+    it "if else else branch" $ do
+        let src = [r|
+           procedure main(a) {
+                if (false) {
+                    x = true;
+                } else {
+                    x = false;
+                }
+                return x;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("x", False) ts `shouldBe` True
+
+    it "and" $ do
+        let src = [r|
+           procedure main(a) {
+                ff = false & false;
+                ft = false & true;
+                tf = true & false;
+                tt = true & true;
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("ff", False) ts `shouldBe` True
+        isInAllReturns ("ft", False) ts `shouldBe` True
+        isInAllReturns ("tf", False) ts `shouldBe` True
+        isInAllReturns ("tt", True) ts `shouldBe` True
+
+    it "or" $ do
+        let src = [r|
+           procedure main(a) {
+                ff = false | false;
+                ft = false | true;
+                tf = true | false;
+                tt = true | true;
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("ff", False) ts `shouldBe` True
+        isInAllReturns ("ft", True) ts `shouldBe` True
+        isInAllReturns ("tf", True) ts `shouldBe` True
+        isInAllReturns ("tt", True) ts `shouldBe` True
+
+    it "xor" $ do
+        let src = [r|
+           procedure main(a) {
+                ff = false ^ false;
+                ft = false ^ true;
+                tf = true ^ false;
+                tt = true ^ true;
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("ff", False) ts `shouldBe` True
+        isInAllReturns ("ft", True) ts `shouldBe` True
+        isInAllReturns ("tf", True) ts `shouldBe` True
+        isInAllReturns ("tt", False) ts `shouldBe` True
+
+    it "implies" $ do
+        let src = [r|
+           procedure main(a) {
+                ff = false => false;
+                ft = false => true;
+                tf = true => false;
+                tt = true => true;
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("ff", True) ts `shouldBe` True
+        isInAllReturns ("ft", True) ts `shouldBe` True
+        isInAllReturns ("tf", False) ts `shouldBe` True
+        isInAllReturns ("tt", True) ts `shouldBe` True
+
+
+    it "equal" $ do
+        let src = [r|
+           procedure main(a) {
+                ff = false == false;
+                ft = false == true;
+                tf = true == false;
+                tt = true == true;
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("ff", True) ts `shouldBe` True
+        isInAllReturns ("ft", False) ts `shouldBe` True
+        isInAllReturns ("tf", False) ts `shouldBe` True
+        isInAllReturns ("tt", True) ts `shouldBe` True
+
+    it "readbool" $ do
+        let src = [r|
+           procedure main(a) {
+                car = true;
+                other = read_bool();
+                yet_another = read_bool();
+                yet_another_again = read_bool();
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("car", True) ts `shouldBe` True
+
+    it "printbool" $ do
+        let src = [r|
+           procedure main(a) {
+                car = true;
+                print_bool(true);
+                print_bool(false);
+                return a;
+            } 
+        |]
+        let ast = fromRight (error "Expected Right but got Left") $ parse parseMiniMM "internal.txt" src
+        let ts = fromRight (error "Expected Right but got Left") (compileMiniMM ast)  
+        isInAllReturns ("car", True) ts `shouldBe` True
